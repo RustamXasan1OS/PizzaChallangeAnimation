2247303f587ab2b0a15cc127c8027aeb5ac201c9
RustamXasan1OS <rustamxasan1@gmail.com>
2022-11-26 20:37:44 -0800

    Initial Commit

---
+++ PizzaChallenge01/PizzaChallenge01App.swift
@@ -0,0 +1,17 @@
+//
+//  PizzaChallenge01App.swift
+//  PizzaChallenge01
+//
+//  Created by Rustam Xasan on 26/11/22.
+//
+
+import SwiftUI
+
+@main
+struct PizzaChallenge01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
